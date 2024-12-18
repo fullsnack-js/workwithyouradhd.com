8b734d7aed6b66cc3b225120fc327a84b8ed3841
fullsnack-js <85705660+fullsnack-js@users.noreply.github.com>
2024-12-18 11:58:15 +0000

    ts hack

--- app/(blog)/features.tsx
+++ app/(blog)/features.tsx
@@ -1,6 +1,6 @@
 import { CalendarDaysIcon, VideoCameraIcon, EnvelopeIcon, ClockIcon, UserGroupIcon } from '@heroicons/react/24/outline'
 
-const features = [
+const features: any = [
   {
     name: 'Weekly Video Sessions',
     description:
@@ -49,7 +49,7 @@ export default function Features() {
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            {features.map((feature) => (
+            {features.map((feature:any) => (
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-base font-semibold leading-7 text-gray-900">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-orange-600">
